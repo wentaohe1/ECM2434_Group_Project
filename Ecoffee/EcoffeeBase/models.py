3d75576b5b73b28e9355a2c9db107b02edccdab4
LokSangKee <lsk206@exeter.ac.uk>
2025-02-09 22:41:27 +0000

    database version 1

--- Ecoffee/EcoffeeBase/models.py
+++ Ecoffee/EcoffeeBase/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
+class Shop(models.Model):
+    shopId = models.AutoField(primary_key=True)
+    shopName = models.CharField(max_length=255, unique=True)
+    numberOfVisits = models.IntegerField(
+        validators=[MinValueValidator(0)]
+    )
+
+
+class Coffee(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    numberOrdered = models.IntegerField(
+        validators=[MinValueValidator(0)]
+    )
+    lastOrdered = models.DateTimeField(null=True, blank=True)
+
+
+class Badge(models.Model):
+    badgeId = models.AutoField(primary_key=True)
+    """maybe information like desc or total owned? """
+
+
+class User(models.Model):
+    userId = models.AutoField(primary_key=True)
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
+    cupsSaved = models.IntegerField(
+        validators=[MinValueValidator(0)]
+    )
+    mostRecentShopId = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    progression = models.IntegerField(
+        validators=[MinValueValidator(0),MaxValueValidator(100)]
+    )
+    defaultBadgeId = models.ForeignKey(Badge, on_delete=models.CASCADE)
+    lastActiveDateTime = models.DateTimeField(null=True, blank=True)
+
+
+class UserShop(models.Model):
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)
+    shopId = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    visitAmounts = models.IntegerField(
+        validators=[MinValueValidator(0)]
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["userId", "shopId"], name="uniqueUserShop")
+        ]
+
+
+class UserBadge(models.Model):
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)
+    badgeId = models.ForeignKey(Badge, on_delete=models.CASCADE)
+    owned = models.BooleanField(default=False)
+    dateTimeObtained = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["userId", "badgeId"], name="uniqueUserBadge")
+        ]
