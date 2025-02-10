18a81f0519aa133b552e7fddb0cecd34d5d0e33b
LokSangKee <lsk206@exeter.ac.uk>
2025-02-10 17:22:50 +0000

    Add files via upload

--- Ecoffee/EcoffeeBase/models.py
+++ Ecoffee/EcoffeeBase/models.py
@@ -9,6 +9,7 @@ class Shop(models.Model):
     numberOfVisits = models.IntegerField(
         validators=[MinValueValidator(0)]
     )
+    activeCode = models.CharField(max_length=255)
 
 
 class Coffee(models.Model):
@@ -21,6 +22,9 @@ class Coffee(models.Model):
 
 class Badge(models.Model):
     badgeId = models.AutoField(primary_key=True)
+    coffeeUntilEarned = models.IntegerField(
+        validators=[MinValueValidator(0)]
+    )
     """maybe information like desc or total owned? """
 
 
@@ -31,11 +35,11 @@ class User(models.Model):
     cupsSaved = models.IntegerField(
         validators=[MinValueValidator(0)]
     )
-    mostRecentShopId = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    mostRecentShopId = models.ForeignKey(Shop, null=True, blank=True, on_delete=models.SET_NULL)
     progression = models.IntegerField(
         validators=[MinValueValidator(0),MaxValueValidator(100)]
     )
-    defaultBadgeId = models.ForeignKey(Badge, on_delete=models.CASCADE)
+    defaultBadgeId = models.ForeignKey(Badge, null=True, blank=True, on_delete=models.SET_NULL)
     lastActiveDateTime = models.DateTimeField(null=True, blank=True)
 
 
