a3c15adfb60755cded7b62ed12274962b512f542
YukYinFung <fyy724yuk@gmail.com>
2025-02-10 19:37:26 +0000

    Debug urls

--- Ecoffee/login_system/views.py
+++ Ecoffee/login_system/views.py
@@ -3,27 +3,34 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 
-def login_user(request):
-    if request.method=="POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect('home')
-
         else:
-            messages.success(request,("There was an error logging in, Try again"))
+            messages.success(request, "There was an error logging in, Try again")
             return redirect('login')
+    return render(request, 'registration/login.html')
 
-    else:
-        return render (request, 'authenticate/login.html',{})
-
-
-def logout_user(request):
+def logout_view(request):
     logout(request)
-    messages.success(request,("Logout successful"))
+    messages.success(request, "Logout successful")
     return redirect('home')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
 
 def register_user(request):
     if request.method=="POST":
@@ -40,17 +47,6 @@ def register_user(request):
         form=UserCreationForm()
 
     return render(request, 'authenticate/register_user.html',{'form':form,})
-
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('home')
-    else:
-        form = UserCreationForm()
-    return render(request, 'registration/register.html', {'form': form})
 
 def login_view(request):
     if request.method == 'POST':
