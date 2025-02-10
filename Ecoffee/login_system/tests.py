3286dfa700c92eb9e68331e066b7eadb2da67c76
YukYinFung <fyy724yuk@gmail.com>
2025-02-10 19:42:55 +0000

    add tests

--- Ecoffee/login_system/tests.py
+++ Ecoffee/login_system/tests.py
@@ -1,4 +1,34 @@
 # Remove unused import if not needed
 # from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+class LoginSystemTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpass123'
+        )
+
+    def test_login_view_get(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/login.html')
+
+    def test_login_view_post_success(self):
+        response = self.client.post(reverse('login'), {
+            'username': 'testuser',
+            'password': 'testpass123'
+        })
+        self.assertRedirects(response, reverse('home'))
+
+    def test_login_view_post_failure(self):
+        response = self.client.post(reverse('login'), {
+            'username': 'testuser',
+            'password': 'wrongpass'
+        })
+        self.assertRedirects(response, reverse('login'))
