acf71d3cdaf514a9e74e47714146efd9cfb556be
YukYinFung <fyy724yuk@gmail.com>
2025-02-12 20:44:29 +0000

    fix spacing and lines

--- Ecoffee/Ecoffee/settings.py
+++ Ecoffee/Ecoffee/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oz2q3mh=x*j(k_asvla@gk^@__z1dob2_&#gp6s7m3h)r!b-90'
+SECRET_KEY = (
+    'django-insecure-oz2q3mh=x*j(k_asvla@gk^@__z1dob2_&#gp6s7m3h)r!b-90'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -91,16 +93,21 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 
+            'django.contrib.auth.password_validation.'
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 
+            'django.contrib.auth.password_validation.'
+
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 
+            'django.contrib.auth.password_validation.'
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 
+            'django.contrib.auth.password_validation.',
     },
 ]
 
--- Ecoffee/Ecoffee/urls.py
+++ Ecoffee/Ecoffee/urls.py
@@ -19,8 +19,8 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',views.home,name='home'),
-    path('login_system/',include('django.contrib.auth.urls')),
-    path('login_system/',include('login_system.urls')),
+    path('admin/', admin.site.urls), 
+    path('',views.home,name='home'), 
+    path('login_system/',include('django.contrib.auth.urls')), 
+    path('login_system/',include('login_system.urls')), 
 ]
