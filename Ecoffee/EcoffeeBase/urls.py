962c4fa7b31213ccf029ab943d7e0e46514287f6
Retr0-matrix <bp415@exeter.ac.uk>
2025-02-16 18:01:21 +0000

    urls.py

--- Ecoffee/EcoffeeBase/urls.py
+++ Ecoffee/EcoffeeBase/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index)
+    path('', views.index, name='index'),
+    path('get-params/', views.get_params, name='get_params'),
+    path('redirect/', views.redirect_view, name='redirect'),
 ]
