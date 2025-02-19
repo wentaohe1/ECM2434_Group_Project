aad59ac92d97a9ae20b8a373140aae11dbe77d5e
LukeThack <lopthacker@gmail.com>
2025-02-19 20:35:13 +0000

    Added the functionality for users  to choose the coffee they ordered (from the options in the database) and then read/save the relevent data to the models

---
+++ Ecoffee/login_system/signals.py
@@ -0,0 +1,15 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from EcoffeeBase.models import CustomUser
+
+
+@receiver(post_save, sender=User)
+def create_custom_user(sender, instance, created, **kwargs):
+    if created:
+        CustomUser.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_custom_user(sender, instance, **kwargs):
+    instance.customuser.save()
--- Ecoffee/qr_codes/views.py
+++ Ecoffee/qr_codes/views.py
@@ -2,43 +2,48 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from EcoffeeBase.models import *
 from datetime import datetime
+from .form import CoffeeForm
+
+
 def receive_code(request):
-    code = str(request.GET.get('code', 'No code provided'))
-    try:
-        shop_name=code[:4]
-        coffee_code=code[4:8]
-        shop_code=code[8:12]
-
-
-        #find part of the code which is relavent to the shop/coffee
-        shop=Shop.objects.get(activeCode=shop_code)
-        coffee=Coffee.objects.get(name=coffee_code)
-        coffee.numberOrdered+=1
-        coffee.lastOrdered=datetime.now()
-        shop.numberOfVisits+=1
-        shop.save()
-        coffee.save()
-        if request.user.is_authenticated:
+    if request.method=='POST':
+        form=CoffeeForm(request.POST)
+        if form.is_valid():
+            coffee=form.cleaned_data['coffee_selected']
+            coffee.numberOrdered+=1
+            coffee.lastOrdered=datetime.now()
+            code = str(request.GET.get('code', 'No code provided'))
             try:
-                username=request.user
-                user=User.objects.get(user=username)
-                user.cupsSaved+=1
-                user.mostRecentShopId=shop
-                user.progression+=1
-                user.lastActiveDateTime=datetime.now()
-                #need to run a trigger to check if the badge needs to be updated.
-                user.save()
-            except User.DoesNotExist:
+                #find part of the code which is relavent to the shop
+                shop_code=read_shop_code(code,4)#for now, first 4 letters, allows for expansion later.
+                print(shop_code)
+                shop=Shop.objects.get(activeCode=int(shop_code))
+                shop.numberOfVisits+=1
+                if request.user.is_authenticated:
+                    try:
+                        username=request.user #Updates information relevant to the users order.
+                        user=CustomUser.objects.get(user=username)
+                        user.cupsSaved+=1
+                        user.mostRecentShopId=shop
+                        user.lastActiveDateTime=datetime.now()
+                        #need to run a trigger to check if the badge needs to be updated.
+                        user.save()
+                        shop.save()
+                        coffee.save() #saves after everything is confirmed okay, changes will rollback (by default after a request has returned) if not
+                        return redirect('home')
+                    except CustomUser.DoesNotExist:
+                        return redirect('register')
+                else:
+                    #user not logged in has to login/create account
+                    return redirect('login')
+            except Shop.DoesNotExist:
                 return redirect('home')
-        else:
-            #user not logged in has to login/create account
-            return redirect('login')
+    
+    coffees=Coffee.objects.all()
+    context={'coffees':coffees, 'form':CoffeeForm()}
+    return render(request,'add_coffee.html',context)
         
 
 
-    except Shop.DoesNotExist:
-        return redirect('home')
-    
-
-    
-    return HttpResponse(f"Received code: {code}")
+def read_shop_code(code,number_of_letters):
+    return code[:number_of_letters]
