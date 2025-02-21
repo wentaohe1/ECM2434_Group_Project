5e6407342f26259bd6f927d007afa73adad4f04b
John Doe <grayshondaniel@gmail.com>
2025-02-21 17:18:13 +0000

    Daniel G: tests if more database objects corretcly update

--- Ecoffee/EcoffeeBase/tests.py
+++ Ecoffee/EcoffeeBase/tests.py
@@ -5,18 +5,21 @@ from views import log_visit
 
 class DataBaseTests(TestCase):
     @classmethod
-    def setUpTestData(cls):
+    def set_up_test_data(cls):
         '''Sets up a DB with test objects'''
         cls.obj = Shop.objects.create(firstName = 'John', lastName = 'Smith')
         cls.obj = User.objects.create(shopName = 'New Shop', activeCode = '123')
-        cls.obj = Badge.objects.create()
         cls.obj = Coffee.objects.create(name = 'espresso')
 
-    def setUp(self):
+        # Initialises badges: different thresholds enable user.defaultBadgeId testing
+        cls.obj = Badge.objects.create(coffeeUntilEarned = 1)
+        cls.obj = Badge.objects.create(coffeeUntilEarned = 2)
+
+    def set_up(self):
         '''Sets up a simulated POST request'''
         self.request = self.client.post(reverse(), {'userId' : 1, 'shopId' : 1, 'coffeeName' : 'espresso'}) # sort out reverse
 
-    def testShopRegistersVisit(self, cls):
+    def test_shop_registers_visit(self, cls):
         '''Tests log_visit updates Shop attributes to reflect user's visit'''
 
         # Simulates recieving the POST request
@@ -26,7 +29,7 @@ class DataBaseTests(TestCase):
 
         TestCase.assertEqual(this_shop.numberOfVisits, 1)
 
-    def testUserRegistersVisit(self, cls):
+    def test_user_registers_visit(self, cls):
         '''Tests log_visit updates User attributes to reflect a shop visit'''
 
         log_visit(self.request)
@@ -40,6 +43,57 @@ class DataBaseTests(TestCase):
         TestCase.assertEqual(this_user.cupsSaved, 1)
 
         #todo: check that time of visit = almost now
+
+    def test_user_shop_registers_visit(self, cls):
+        '''Tests log_visit updates UserShop attributes to reflect a user's visit'''
+
+        log_visit(self.request)
+
+        this_userShop = cls.objShop.objects.get(userId = 1, shopId = 1)
+
+        TestCase.assertEqual(this_userShop.userId, 1)
+
+        TestCase.assertEqual(this_userShop.shopId, 1)
+
+        TestCase.assertEqual(this_userShop.visitAmounts, 1)
+
+    def test_user_shop_registers_visit(self, cls):
+        '''Tests log_visit updates UserShop attributes to reflect a user's visit'''
+
+        log_visit(self.request)
+
+        this_badge = cls.objShop.objects.get(badgeId = 1) # 1st badge
+        this_user = cls.objShop.objects.get(userId = 1)
+        UserBadge = cls.objShop.objects.get(badgeId = 1, userId = 1)
+
+        # Tests user fields updated
+        TestCase.assertEqual(this_user.progression, 0)
+
+        TestCase.assertEqual(this_user.defaultBadgeId, this_badge.badgeId)
+
+        # Tests user_badge fields updated
+        TestCase.assertEqual(this_user.owned, True)
+
+        #test that badge was earned 'now'
+
+    def test_user_default_badge_updates_when_new_badge_won(self, cls):
+        '''Tests that the default badge ID changes when the user earns a new badge'''
+
+        # Logs enough visits for both 1st and 2nd badges to be owned
+        log_visit(self.request)
+        log_visit(self.request)
+
+        badge_2 = cls.objShop.objects.get(badgeId = 2) # 2nd badge
+        this_user = cls.objShop.objects.get(userId = 1)
+
+        TestCase.assertEqual(this_user.defaultBadgeId, badge_2.badgeId)
+
+
+
+
+
+    
+
 
 
 # Tests for EcoffeeBase
