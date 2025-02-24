38cad8b037a651938b858d1f9fe04c17a79ddbe4
DanielJGrayshon <grayshondaniel@gmail.com>
2025-02-24 22:27:09 +0000

    updating urls.py

--- Ecoffee/EcoffeeBase/urls.py
+++ Ecoffee/EcoffeeBase/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('log_visit/', views.log_visit, name = 'log_visit'),
+    path('log_visit/', views.log_visit, name = 'log_visit')
 ]
 
