8b9f4dc1b909a92c5751521d4df3f5783401faaf
DanielJGrayshon <grayshondaniel@gmail.com>
2025-02-25 11:22:46 +0000

    Revert "Update views.py to snake case"
    This reverts commit 9f16a010b47537c096e12ee182a11089f258e080.

--- Ecoffee/EcoffeeBase/views.py
+++ Ecoffee/EcoffeeBase/views.py
@@ -49,6 +49,7 @@ def log_visit(request):
 
             custom_user.default_badge_id = badge
 
+            user_badge.owned = True # Badge is only created when obtained
             user_badge.date_time_obtained = now()
             user_badge.save()
     custom_user.save()
@@ -59,17 +60,17 @@ def home(request):
     today = now().date()
 
     # Calculate cups saved today
-    cups_saved_today = CustomUser.objects.filter(last_active_date_time=today).aggregate(total=Sum('cups_saved'))['total'] or 0
+    cups_saved_today = custom_user.objects.filter(last_active_date_time=today).aggregate(total=Sum('cups_saved'))['total'] or 0
     total_cups_saved=Shop.objects.all().aggregate(total=Sum('number_of_visits'))['total'] or 0
     # Set a daily goal
     daily_goal = 100  
-    request_user=CustomUser.objects.get(user=request.user)
+    request_user=custom_user.objects.get(user=request.user)
     personal_cups_saved=request_user.cups_saved
     # Calculate progress percentage
     progress_percentage = (cups_saved_today / daily_goal) * 100 if daily_goal else 0
     progress_percentage = min(progress_percentage, 100)  # Ensure it doesn’t exceed 100%
     # Function to determine user badge path
-    top_10_users= CustomUser.objects.all().order_by('-cups_saved')[:10] #top 10 users ordered in descending order
+    top_10_users= custom_user.objects.all().order_by('-cups_saved')[:10] #top 10 users ordered in descending order
     top_5_shops=Shop.objects.all().order_by('-number_of_visits')[:5]
     return render(request, 'homepage.html', {
         'cups_saved_today': cups_saved_today,
@@ -84,7 +85,7 @@ def home(request):
 
 def dashboard_view(request):
     if request.user.is_authenticated:
-        request_user=CustomUser.objects.get(user=request.user)
+        request_user=custom_user.objects.get(user=request.user)
         coffees_saved=request_user.cups_saved
         if request_user.default_badge_id!=None:
             user_badge='images/'+str(request_user.default_badge_id.badge_image)
@@ -116,7 +117,7 @@ def get_next_badge(request_user):
     ordered_badges=Badge.objects.order_by('coffee_until_earned')
 
     for badge in ordered_badges:
-        if badge.coffee_until_earned>current_badge.coffee_until_earned:
+        if badge.coffee_until_earned>current_badge.coffeeUntilEarned:
             return badge
 def welcome(request):
     return render(request, 'welcome.html')
