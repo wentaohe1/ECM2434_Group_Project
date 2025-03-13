2d119a0096c22de7290b90e04f06cb94fd9265b5
Retr0-matrix <bp415@exeter.ac.uk>
2025-03-13 11:46:50 +0000

    Update urls.py

--- Ecoffee/Ecoffee/urls.py
+++ Ecoffee/Ecoffee/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 from . import views, settings
 from Ecoffee.views import dashboard_view
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
