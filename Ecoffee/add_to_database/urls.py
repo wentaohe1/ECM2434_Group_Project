926324aa35fc16f98da8372a53e61685f127d792
Retr0-matrix <bp415@exeter.ac.uk>
2025-03-20 19:29:49 +0000

    Update urls.py

--- Ecoffee/add_to_database/urls.py
+++ Ecoffee/add_to_database/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('add_shop', views.add_shop, name='add_shop'),
     path('add_coffee', views.add_coffee, name='add_coffee'),
     path('add_badge', views.add_badge, name='add_badge'),
+    path('shop_owner/<int:shop_id>', views.shop_owner, name='shop_owner'),
+    path('upload_logo/<int:shop_id>', views.upload_logo, name='upload-logo'),
+    path('', views.receive_code, name='receive_code'),
 ]
