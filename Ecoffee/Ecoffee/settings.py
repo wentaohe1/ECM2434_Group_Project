b125b63eeaa4a330c681b4eca4658178131535bc
wentaohe1 <146285279+wentaohe1@users.noreply.github.com>
2025-03-22 19:03:25 +0000

    changed the home page a little bit and made pages mobile responsive

--- Ecoffee/Ecoffee/settings.py
+++ Ecoffee/Ecoffee/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.16.23.84']
 
 
 # Application definition
