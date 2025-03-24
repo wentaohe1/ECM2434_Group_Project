0197e9de1ad19921126d96773c455997841751ac
Alexander163 AlexanderDyer163@proton.me <AlexanderDyer163@proton.me>
2025-03-24 16:25:10 +0000

    Added a render.yaml file for a last ditch attempt

--- Ecoffee/Ecoffee/settings.py
+++ Ecoffee/Ecoffee/settings.py
@@ -133,8 +133,12 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
+
+STATICFILES_DIRS = [
+    os.path.join(Path(__file__).resolve().parent, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
