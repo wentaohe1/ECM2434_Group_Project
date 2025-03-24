7bbfe31152fa3665c71a2d578d022d06b627be30
Alexander163 AlexanderDyer163@proton.me <AlexanderDyer163@proton.me>
2025-03-24 15:38:39 +0000

    Added a render.yaml file for a last ditch attempt

--- Ecoffee/Ecoffee/settings.py
+++ Ecoffee/Ecoffee/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'Ecoffee/staticfiles')
 
 
 STATICFILES_DIRS = [
